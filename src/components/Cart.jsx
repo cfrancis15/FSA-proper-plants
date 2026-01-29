76bb483602993314ad1c727559420ec181ba6879
Connor Francis <connorfrancis@live.com>
2026-01-29 11:35:19 -0700

    functionality is set

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -5,13 +5,38 @@ import { useState } from "react"
 export default function Cart({cartItems}){
     
 
-    console.log(cartItems)
+    let itemCounts = {}
+    cartItems.forEach(item=>{
+        itemCounts[item.name] = (itemCounts[item.name] || 0) +1;
+
+    })
+
+    const uniqueItems = cartItems.filter((item,index)=>
+        index === cartItems.findIndex(t=>t.name===item.name)
+    )
+    
+    
+
 
     return(
         <>
             <div className="cart-box">
-                <p>HELLO</p>
+                <h3>Items in Cart</h3>
+                {uniqueItems.map((item)=>(
+
+                    <p key={item.name}>{item.name} {item.image} Count:{itemCounts[item.name]}
+                    </p>
+                    
+
+                ))
+                    
+                }
+
+                
+                
             </div>
+
+            
         </>
     )
 }
